315713874578117c6eaa8f530e7da79043f722d4
Ignacio246 <50604781+Ignacio246@users.noreply.github.com>
2019-06-05 14:47:45 -0500

    Delete example01.py

--- example01.py
+++
@@ -1,9 +0,0 @@
-number_01=10.5
-number_02=0
-if (number_01 >= number_02):
-    add=number_01 + number_02
-    print "Add", add
-else:
-    division == number_01/number_02
-    print (division)
-print ("end")
