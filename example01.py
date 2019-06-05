b649eab366de0feefed03dfb46eb6b873cf8b910
Ignacio246 <50604781+Ignacio246@users.noreply.github.com>
2019-06-05 14:22:34 -0500

    Add files via upload

---
+++ example01.py
@@ -0,0 +1,9 @@
+number_01=10.5
+number_02=0
+if (number_01 >= number_02):
+    add=number_01 + number_02
+    print "Add", add
+else:
+    division == number_01/number_02
+    print (division)
+print ("end")
