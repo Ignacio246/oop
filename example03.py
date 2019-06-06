1c5a7b9598bb203db8e414f652ee7ce1a3fe7dc3
Ignacio246 <50604781+Ignacio246@users.noreply.github.com>
2019-06-06 10:53:11 -0500

    Add files via upload
    Bucle for

---
+++ example03.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print("Hola mundo", i)
+numbers=['Uno', 'Dos', 'Tres']
+for i in numbers:
+    print("Hola mundo", i)
+numbers=[10.5 , 14, 18.23]
+for i in numbers:
+    print("Hola mundo", (i+1))
+numbers=[10.5,14,18.23,]
+for i in numbers:
+    print((i+1),"\nBye")
